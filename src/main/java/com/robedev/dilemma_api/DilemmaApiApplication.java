e8b8862dcef7e0dba6fbf86fcd16b9e2af15272a
Roberto Vetere <Roberto.vetere@gmail.com>
2025-04-30 18:57:07 +0200

    ADD structure start.spring.io

---
+++ src/main/java/com/robedev/dilemma_api/DilemmaApiApplication.java
@@ -0,0 +1,13 @@
+package com.robedev.dilemma_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DilemmaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DilemmaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robedev/dilemma_api/DilemmaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robedev.dilemma_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DilemmaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
